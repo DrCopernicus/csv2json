821cfc3c384f4a05b0e74aa026a6401157de8935
DrCopernicus <nclee2@asu.edu>
2015-04-18 21:43:30 -0700

    support for nested lists

--- csv2json.py
+++ csv2json.py
@@ -6,27 +6,31 @@ import string
 # i deserve an oscar or something
 
 
-filename = raw_input('Enter file without ext: ')
-output = {filename: []}
-
 def add_to_output(outlist, cat, value):
     if isinstance(cat, str) or len(cat) == 1:
         outlist[cat[0]] = value
     else:
         if cat[0] not in outlist:
             outlist[cat[0]] = {}
-        tiger = outlist[cat[0]] # why is this called tiger?
+        tiger = outlist[cat[0]]  # why is this called tiger?
         add_to_output(tiger, cat[1:], value)
 
-with open(filename+'.csv') as csv_file:
-    for molecule in csv.DictReader(csv_file): # and why is this called molecule??
-        outarea = {}
-        for key in molecule:
-            add_to_output(outarea, string.split(key,'.'), molecule[key])
-        output[filename].append(outarea)
+def make_a_list(filename):
+    output = {filename: []}
+    with open(filename+'.csv') as csv_file:
+        for molecule in csv.DictReader(csv_file):  # and why is this called molecule??
+            outarea = {}
+            for key in molecule:
+                if molecule[key].startswith('$$FILE.'):
+                    add_to_output(outarea, string.split(key,'.'), make_a_list('thecreators'))
+                else:
+                    add_to_output(outarea, string.split(key,'.'), molecule[key])
+            output[filename].append(outarea)
+    return output[filename]
 
-output_json = json.dumps(output[filename], sort_keys=True, indent=2)
+superfilename = raw_input('Enter file without ext: ')
+superout = make_a_list(superfilename)
 
-f = open(filename+'.json','w')
-f.write(output_json)
+f = open(superfilename+'.json','w')
+f.write(json.dumps(superout, sort_keys=True, indent=2))
 f.close()
